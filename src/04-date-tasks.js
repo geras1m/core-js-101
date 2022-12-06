9073c2787107e9d35ee02560278de034724a4f93
Anton Haldouski <anton94-94@list.ru>
2022-12-06 22:42:10 +0300

    feat: solutions (not all) of 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
 }
 
 
@@ -73,10 +74,25 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const difference = new Date(endDate - startDate);
+  const hours = difference.getUTCHours();
+  const hoursStr = hours < 10 ? `0${hours}` : hours;
+  const minutes = difference.getMinutes();
+  const minutesStr = minutes < 10 ? `0${minutes}` : minutes;
+  const seconds = difference.getSeconds();
+  const secondsStr = seconds < 10 ? `0${seconds}` : seconds;
+  const milliseconds = difference.getMilliseconds();
+  let millisecondsStr;
+  if (milliseconds < 10) {
+    millisecondsStr = `${milliseconds}00`;
+  } else if (milliseconds < 100) {
+    millisecondsStr = `${milliseconds}0`;
+  } else {
+    millisecondsStr = `${milliseconds}`;
+  }
+  return `${hoursStr}:${minutesStr}:${secondsStr}.${millisecondsStr}`;
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
